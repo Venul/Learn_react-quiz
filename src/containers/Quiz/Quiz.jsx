5b19d4d5c1c46cd3cd1397f050e9d08cc72d169a
Lunev Sergey <s.lunev88@gmail.com>
2021-12-07 23:21:17 +0300

    6.12: add finished Quiz

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import classes from './Quiz.module.css'
-import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz.jsx'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 
 class Quiz extends Component {
   state = {
+    isFinished: true,
     activeQuestion: 0,
     answerState: null,
     quiz: [
@@ -33,17 +35,25 @@ class Quiz extends Component {
   }
 
   onAnswerClickHandler = answerId => {
+    if (this.state.answerState) {
+      const value = Object.values(this.state.answerState)[0]
+      if (this.state.answerState[value] === 'success') {
+        return
+      }
+    }
+
     const question = this.state.quiz[this.state.activeQuestion]
     
     if (question.rightAnswerId === answerId) {
-      
       this.setState({
         answerState: {[answerId]: 'success'}
       })
 
       const timeout = window.setTimeout(() => {
         if (this.isQuizFinished()) {
-          console.log('Finished')
+          this.setState({
+            isFinished: true
+          })
         } else {
           this.setState({
             activeQuestion: this.state.activeQuestion + 1,
@@ -68,7 +78,11 @@ class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Answer all questions</h1>
-          <ActiveQuiz 
+
+          {
+            this.state.isFinished
+            ? <FinishedQuiz />
+            : <ActiveQuiz 
             answers={this.state.quiz[this.state.activeQuestion].answers}
             question={this.state.quiz[this.state.activeQuestion].question}
             onAnswerClick={this.onAnswerClickHandler}
@@ -76,6 +90,8 @@ class Quiz extends Component {
             answerNumber={this.state.activeQuestion + 1}
             state={this.state.answerState}
           />
+          }
+
         </div>
       </div>
     )
